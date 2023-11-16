6964daa59484329e5017e15874e794923744d7a6
Aiswarya-A-M <aiswarya.am@neoito.com>
2023-11-16 14:50:24 +0530

    formatted

--- register.js
+++ register.js
@@ -3,58 +3,57 @@ import fs from "fs";
 import { v4 as uuidv4 } from "uuid";
 import { error } from "console";
 
-const DBPath = "./DB.json";
+const detailsStorePath = "./detailsStore.json";
 const errorFilePath = "./error.txt";
-
+const empInfo = getDetails();
 console.log("welcome");
-let startTime = new Date().getMinutes();
+const startTime = new Date().getMinutes();
 showOptions();
 
 function showOptions() {
-  console.log("1. Create new employee");
-  console.log("2. Update employee record");
-  console.log("3. Delete employee");
-  console.log("4.Display employees by department");
-  console.log("5.Display employee by employee id");
-  console.log("6.exit");
-  const option = readlineSync.question("choose option");
-  performFunctions(option);
+  console.log("1. Create new employee ");
+  console.log("2. Update employee record ");
+  console.log("3. Delete employee ");
+  console.log("4. Display employees by department ");
+  console.log("5. Display employee by employee id ");
+  console.log("6. exit ");
+  const option = readlineSync.question("choose option ");
+  chooseOption(option);
 }
 
-function performFunctions(option) {
+function chooseOption(option) {
   switch (option) {
     case "1":
-      addData();
+      addEmployeeDetails();
       break;
     case "2":
-      updateData();
+      updateEmployeeDetails();
       break;
     case "3":
-      deleteDetails();
+      deleteEmployeeDetails();
       break;
     case "4":
-      viewDetails();
+      viewDepartmentDetails();
       break;
     case "5":
-      viewDepartmentDetails();
+      viewEmployeeDetails();
       break;
     case "6":
       exitFromSession();
       break;
-
     default:
       showOptions();
   }
 }
 
-function addData() {
+function addEmployeeDetails() {
   try {
     const empName = readlineSync.question("Please enter your name");
     const empDOB = readlineSync.question(
       "Please enter your date of birth dd/mm/yyyy"
     );
     const empDepartment = readlineSync.question("Please enter your department");
-    const age = findAge(empDOB);
+    const age = getAge(empDOB);
     const details = {
       empId: uuidv4(),
       empName: empName,
@@ -62,25 +61,24 @@ function addData() {
       empDepartment: empDepartment,
       empAge: age,
     };
-    const empInfo = takeDetails();
     const employeeIndex = empInfo.findIndex(
       (obj) => obj.empName === details.empName
     );
 
     if (employeeIndex !== -1) {
       console.log("empName Exist");
-    } else {
-      empInfo.push(details);
-      addToDB(empInfo);
-      console.log("Employee details added successfully");
+      return;
     }
+    empInfo.push(details);
+    addDetails(empInfo);
+    console.log("Employee details added successfully");
   } catch (error) {
-    console.log(`something went wrong taking details from DB ${error.message}`);
+    console.log(`something went wrong taking details ${error.message}`);
     errorWriting(error);
   }
 }
 
-function findAge(dob) {
+function getAge(dob) {
   const dobArray = dob.split("/");
   const birthDate = new Date(dobArray[2], dobArray[1] - 1, dobArray[0]);
   const currentDate = new Date();
@@ -88,9 +86,9 @@ function findAge(dob) {
   return age;
 }
 
-function takeDetails() {
+function getDetails() {
   try {
-    const data = fs.readFileSync(DBPath, "utf8");
+    const data = fs.readFileSync(detailsStorePath, "utf8");
     return JSON.parse(data);
   } catch (error) {
     console.log(`something went wrong taking details from DB ${error.message}`);
@@ -98,9 +96,9 @@ function takeDetails() {
   }
 }
 
-function addToDB(empInfo) {
+function addDetails(empInfo) {
   try {
-    fs.writeFileSync(DBPath, JSON.stringify(empInfo));
+    fs.writeFileSync(detailsStorePath, JSON.stringify(empInfo));
   } catch {
     errorWriting(error);
     console.log(`error occurred while adding details to DB-${error.message}`);
@@ -108,17 +106,16 @@ function addToDB(empInfo) {
 }
 
 function errorWriting(error) {
-  let today = new Date();
+  const today = new Date();
   today.setTime(today.getTime());
-  let currentTime = today.toUTCString();
-  let errorDetails = `${error.message}----time:${currentTime}\n`;
+  const currentTime = today.toUTCString();
+  const errorDetails = `${error.message}----time:${currentTime}\n`;
   fs.appendFileSync(errorFilePath, errorDetails);
 }
 
-function updateData() {
+function updateEmployeeDetails() {
   try {
-    const empId = readlineSync.question("Please enter employee id");
-    const empInfo = takeDetails();
+    const empId = readlineSync.question("Please enter the employee id");
     const updatedEmployee = empInfo.find(
       (employee) => employee.empId === empId
     );
@@ -128,46 +125,56 @@ function updateData() {
         `please enter name  ${updatedEmployee.empName}: `
       );
       const updatedDOB = readlineSync.question(
-        ` please enter DOB in dd/mm/yyyy format ${updatedEmployee.empDOB}: `
+        ` please enter DOB (dd/mm/yyyy) ${updatedEmployee.empDOB}: `
       );
       const updatedDepartment = readlineSync.question(
         ` please enter department ${updatedEmployee.empDepartment}: `
       );
-      const age = findAge(updatedDOB);
+      const age = getAge(updatedDOB);
       updatedEmployee.empAge = age;
       updatedEmployee.empName = updatedName;
       updatedEmployee.empDOB = updatedDOB;
       updatedEmployee.empDepartment = updatedDepartment;
-      addToDB(empInfo);
+      addDetails(empInfo);
       console.log("Employee details updated successfully");
+      return;
     }
+    console.log("no employee exist");
   } catch (error) {
     console.log(`something went wrong updating details ${error.message}`);
     errorWriting(error);
   }
 }
 
-function deleteDetails() {
+function deleteEmployeeDetails() {
   try {
     const empId = readlineSync.question("Please enter employee id");
-    const empInfo = takeDetails();
     const employeeIndex = empInfo.findIndex(
       (employee) => employee.empId === empId
     );
-    empInfo.splice(employeeIndex, 1);
-    addToDB(empInfo);
-    console.log("delete employee successfully");
+
+    if (employeeIndex !== -1) {
+      empInfo.splice(employeeIndex, 1);
+      addDetails(empInfo);
+      console.log("delete employee successfully");
+      return;
+    }
+    console.log("no employee exist");
   } catch (error) {
     console.log(`something went wrong deleting details ${error.message}`);
     errorWriting(error);
   }
 }
 
-function viewDetails() {
+function viewEmployeeDetails() {
   try {
     const empId = readlineSync.question("Please enter employee id");
-    const empInfo = takeDetails();
     const employee = empInfo.find((employee) => employee.empId === empId);
+
+    if (!employee) {
+      console.log("No employee exist");
+      return;
+    }
     console.log(employee);
   } catch (error) {
     console.log(`something went wrong view details ${error.message}`);
@@ -180,15 +187,21 @@ function viewDepartmentDetails() {
     const empDepartment = readlineSync.question(
       "Please enter employee department"
     );
-    const empInfo = takeDetails();
     let count = 0;
+
     empInfo.forEach((employee) => {
       if (employee.empDepartment === empDepartment) {
         console.log(employee);
         count++;
       }
     });
-    console.log("no of employees in this department", count);
+
+    if (count) {
+      console.log(
+        `number of employees in ${empDepartment} department" , ${count}`
+      );
+    }
+    console.log(`No employees under ${empDepartment} department`);
   } catch (error) {
     console.log(
       `something went wrong view department details ${error.message}`
